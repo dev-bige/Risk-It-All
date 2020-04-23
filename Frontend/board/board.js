19bd0c6755f8c2dd25758e01379a624041cf4063
Matthew G. Hoskins <mgh@iastate.edu>
2020-04-23 15:44:00 -0500

    Full board Setup

--- Frontend/board/board.js
+++ Frontend/board/board.js
@@ -4,6 +4,7 @@ var gameStart = new Array("Skyblue Select", "Red Select", "Orange Select", "Gree
 var tileTitle = new Array("bluesquare", "redsquare", "orangesquare", "greensquare", "violetsquare", "pinksquare");
 var currPhase = -1;
 var currStart = 0;
+var occupiedSpacesSet = 0; //Max for board is 56
 
 function endPhase() {
 	if(currPhase === 11) currPhase = 0;
@@ -13,10 +14,26 @@ function endPhase() {
 
 
 function tilePlay(tileId) {
+	//call board setup code
+	boardSet(tileId);
+}
+
+
+function boardSet(tileId) {
 	//board setup code
 	if (currPhase === -1){
-		document.getElementById(tileId).className = (tileTitle[currStart]);
-		document.getElementById("currentPhase").innerHTML = (gameStart[++currStart]);	
-		if(currStart === 6) currStart = 0;		
+		//check to see if the tile is occupied
+		if (document.getElementById(tileId).className !== "square") alert("Please select an unoccupied tile");
+		else {
+			//Change the class of the tile to change color
+			document.getElementById(tileId).className = (tileTitle[currStart]);			
+			//Change the indicator of what is happening			
+			currStart++;
+			if(currStart === 6) currStart = 0;
+			document.getElementById("currentPhase").innerHTML = (gameStart[currStart]);
+			occupiedSpacesSet++;
+			//check to see if the board is fully setup - if it is it will change phase
+			if (occupiedSpacesSet === 56) document.getElementById("currentPhase").innerHTML = (gamePhase[++currPhase]);			
+		}
 	}
 }
