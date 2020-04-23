99a06624bb7aaf1da4065093f7daaaa7b11bfe1b
Matthew G. Hoskins <mgh@iastate.edu>
2020-04-23 15:16:47 -0500

    Initial color change

--- Frontend/board/board.js
+++ Frontend/board/board.js
@@ -1,8 +1,22 @@
 var gamePhase = new Array("Skyblue Recruit", "Skyblue Attack", "Red Recruit", "Red Attack", "Orange Recruit", "Orange Attack", "Green Recruit", 
 						"Green Attack", "Violet Recruit", "Violet Attack", "Pink Recruit", "Pink Attack");
-var currPhase = 0;
+var gameStart = new Array("Skyblue Select", "Red Select", "Orange Select", "Green Select", "Violet Select", "Pink Select");
+var tileTitle = new Array("bluesquare", "redsquare", "orangesquare", "greensquare", "violetsquare", "pinksquare");
+var currPhase = -1;
+var currStart = 0;
 
 function endPhase() {
 	if(currPhase === 11) currPhase = 0;
-	document.getElementById("currentPhase").innerHTML = (gamePhase[currPhase++]);
+	document.getElementById("currentPhase").innerHTML = (gamePhase[++currPhase]);
+}
+
+
+
+function tilePlay(tileId) {
+	//board setup code
+	if (currPhase === -1){
+		document.getElementById(tileId).className = (tileTitle[currStart]);
+		document.getElementById("currentPhase").innerHTML = (gameStart[++currStart]);	
+		if(currStart === 6) currStart = 0;		
+	}
 }
